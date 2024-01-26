53c06c41ca1e1f11756f0de0d51fc953f3f907d1
HeT02 <het02guagua@gmail.com>
2024-01-26 14:50:58 +0800

    添加数据集                                 2024/1/25 编写数据类文件                       2024/1/26 10:57 编写DNN模型类 定义train、dev、test函数 增加了dim属性，输出了数据集的信息

--- dataset.py
+++ dataset.py
@@ -1,16 +1,17 @@
 import torch
-from torch.utils.data import dataset, dataloader
+from torch.utils.data import Dataset
 import csv
 import numpy as np
-class COVID19Dataset(dataset):
+
+class COVID19Dataset(Dataset):
     def __init__(self,
                  path,
-                 model = 'train',
+                 mode = 'train',
                  target_only = False):
-        self.model = model
+        self.model = mode
 
         # Read data from file
-        with open(path) as f:
+        with open(path, 'r') as f:
             data = list(csv.reader(f))
             # slice the first row and column
             data = np.array(data[1: ])[:, 1:].astype(float)
@@ -20,26 +21,32 @@ class COVID19Dataset(dataset):
         else:
             feats = list(range(40)) + [57, 75]
 
-        if model == 'test':
+        if mode == 'test':
             # Testing data
             # data: 893 * 94
+            data = data[:, feats]
             self.data = torch.FloatTensor(data)
         else:
             # Training data (train/dev sets)
             # data : 2700 * 94, train/dev = 9
             target = data[:, -1]
             data = data[:, feats]
-            if model == 'train':
+            if mode == 'train':
                 # 行索引，train和dev的比例为9：1
                 indices = [i for i in range(len(data)) if i % 10 != 0]
-            elif model == 'dev':
+            elif mode == 'dev':
                 indices = [i for i in range(len(data)) if i % 10 == 0]
             self.data = torch.FloatTensor(data[indices])
             self.target = torch.FloatTensor(target[indices])
 
         # Normalize features
-        self.data[:, 40: ] = (self.data[:, 40: ] - self.data[:, 40:, ].mean(dim=0, keepdim=True)) \
-            / self.data[:, 40: ].std(dim=0, keepdim=True)
+        self.data[:, 40:] = (self.data[:, 40:] - self.data[:, 40:].mean(dim=0, keepdim=True)) \
+            / self.data[:, 40:].std(dim=0, keepdim=True)
+
+        self.dim = self.data.shape[1]
+        print('Finished reading the {} set of COVID19 Dataset ({} samples found, each dim = {})'.format(mode,
+                                                                                                        len(self.data),
+                                                                                                        self.dim))
 
     def __getitem__(self, index):
         # Return one sample at a time
@@ -53,3 +60,5 @@ class COVID19Dataset(dataset):
     def __len__(self):
         # Returns the size of the dataset
         return len(self.data)
+
+
