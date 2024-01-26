c32c81f1f533b6ae2e683ebf2c003bee3013288e
HeT02 <het02guagua@gmail.com>
2024-01-26 11:08:08 +0800

    添加数据集                                 2024/1/25 编写数据类文件                       2024/1/26 10:57 编写DNN模型类

---
+++ model.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+class DNN(nn.Module):
+    def __init__(self, input_dim):
+        super(DNN, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
+        )
+        self.criterion = nn.MSELoss(reduction='mean')
+
+    def forward(self, x):
+        return self.net(x).squeeze(1)
+
+    def cal_loss(self, pred, target):
+        return self.criterion(pred, target)
+
